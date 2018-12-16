104673e0fbb3a4336eb75cab7037ee68c7497793
Andri Wicaksono <andri@zonakreatif.id>
2018-12-16 17:46:33 +0700

    Backend Layouts

---
+++ resources/views/layouts/backend/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  @include('layouts.backend.css')
+</head>
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+    @include('layouts.backend.navbar')
+
+    @include('layouts.backend.left-sidebar')
+
+    @include('layouts.backend.breadcrumb')
+
+    @yield('content')
+    
+    @include('layouts.backend.footer')
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+    </div>
+<!-- ./wrapper -->
+  @include('layouts.backend.script')
+</body>
+</html>

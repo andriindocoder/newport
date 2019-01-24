6a5953cc8ed31891d057f57b9aaacf4ca5c7b03f
Andri Wicaksono <andri@zonakreatif.id>
2019-01-24 11:03:06 +0700

    Alter table informasi

---
+++ database/migrations/alter-table/2019_01_24_034513_alter_informasi.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterInformasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('informasi', function (Blueprint $table) {
+            $table->integer('bulan')->nullable();
+            $table->integer('tahun')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('informasi', function (Blueprint $table) {
+            $table->dropColumn('bulan');
+            $table->dropColumn('tahun');
+        });
+    }
+}

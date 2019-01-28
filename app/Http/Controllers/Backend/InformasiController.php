af851c03aa199c347acc7749835078ec8116538b
Andri Wicaksono <andri@zonakreatif.id>
2019-01-28 11:44:26 +0700

    Reformasi Birokrasi

--- app/Http/Controllers/Backend/InformasiController.php
+++ app/Http/Controllers/Backend/InformasiController.php
@@ -152,6 +152,10 @@ class InformasiController extends BackendController
                     $informasis = Informasi::latest()->jenisInformasi(27)->active()->paginate($perPage);
                     $informasisCount = 1;
                     break;
+                  case 'reformasi-birokrasi' :
+                    $informasis = Informasi::latest()->jenisInformasi(28)->active()->paginate($perPage);
+                    $informasisCount = 1;
+                    break;
                 default:
                     $informasis = Informasi::latest()->paginate($perPage);
                     $informasisCount = 1;
